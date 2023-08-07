3c7a75fc6d8769fe9848e62c3e14422757351236
jiaying.lv <jiaying.lv@qunar.com>
2023-08-08 03:46:37 +0800

    修改文件名为jsx，使用图标

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,4 +1,4 @@
-// import { ContainerOutlined, DesktopOutlined, RobotOutlined } from '@ant-design/icons';
+import { ContainerOutlined, DesktopOutlined, RobotOutlined } from '@ant-design/icons';
 
 export const APPID = 123;
 export const APPKEY = "123";
@@ -9,16 +9,16 @@ export const MENU = [
     {
         label: '简历评估',
         key: 'resume',
-        // icon: <ContainerOutlined />,
+        icon: <ContainerOutlined />,
     },
     {
         label: '模拟面试',
         key: 'interview',
-        // icon: <DesktopOutlined />,
+        icon: <DesktopOutlined />,
     },
     {
         label: '知识问答',
         key: 'answer',
-        // icon: <RobotOutlined />,
+        icon: <RobotOutlined />,
     }
 ];
