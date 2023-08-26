e708319f2dde518be7e44c43e4d7a05730e33839
ticture <ticture@foxmail.com>
2023-08-26 15:47:37 +0800

    feat: add brotli in to bootstrap

--- bootstap.sh
+++ bootstap.sh
@@ -6,6 +6,10 @@ cd /root/JA-FE
 rm -rf dist
 #生成新产物
 vite build
+# 进行brotli压缩
+#find dist -type f \( -iname '*.js' -o -iname '*.css' -o -iname '*.html' \) -exec zopfli --iterations=200 {} \;
+find dist -type f \( -iname '*.js' -o -iname '*.css' -o -iname '*.html' \) -exec brotli -q 11 {} \;
+
 #删除nginx产物
 rm -rf /www/JA-FE/dist
 #复制新产物到nginx
