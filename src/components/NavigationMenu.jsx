e93eac69e8b5ffd5dd9a69c7fca18a44ba27b8ab
LzmUSTB <u202142025@xs.ustb.edu.cn>
2023-09-03 12:49:14 +0800

    修改menu,home,card样式

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, Layout, Button, Avatar, Dropdown } from 'antd';
+import { Menu, Layout, Button, Avatar, Dropdown, ConfigProvider } from 'antd';
 import { useState } from 'react';
 import Login from '../pages/Login/App';
 import { observer } from 'mobx-react';
 import store from '../store';
+import { BgColorsOutlined, } from '@ant-design/icons';
 
 const { Header } = Layout;
 
@@ -15,20 +16,36 @@ const NavigationMenu = () => {
     };
   };
 
-  // const menuItems = [
-  //   {
-  //     key: 'resume',
-  //     label: '简历评估'
-  //   },
-  //   {
-  //     key: 'jobAssistant',
-  //     label: '面试助手'
-  //   },
-  //   {
-  //     key: 'interview',
-  //     label: '模拟面试'
-  //   }
-  // ];
+  const menuItems = [
+    {
+      key: 'home',
+      label: '首页',
+      link: '/'
+    },
+    {
+      key: 'resume',
+      label: '简历评估',
+      link: '/resume'
+    },
+    {
+      key: 'jobAssistant',
+      label: '面试助手',
+      link: '/jobAssistant'
+    },
+    {
+      key: 'interview',
+      label: '模拟面试',
+      link: '/interview'
+    }
+  ];
+
+  const menuItems_component = menuItems.map(item =>
+    <Menu.Item key={item.key}>
+      <Link to={item.link}>
+        {item.label}
+      </Link>
+    </Menu.Item>
+  )
 
   const items = [
     {
@@ -39,57 +56,51 @@ const NavigationMenu = () => {
       type: 'divider'
     },
     {
-      label: (<div onClick={()=>store.deleteJwtToken()}>退出</div>),
+      label: (<div onClick={() => store.deleteJwtToken()}>退出</div>),
       key: 'logout'
     }
   ]
 
   return (
+
     <Header
       style={{
         position: 'sticky',
+        width: '100%',
         top: 0,
         zIndex: 1,
-        width: '100%',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        backgroundColor: '#fff',
       }}
     >
+
       <Link to="/"><div className='page-title'>JobGPT</div></Link>
       <div className='page-menu'>
         <Menu
-          theme="dark"
           mode="horizontal"
           defaultSelectedKeys={[store.currentMenu]}
-          // selectedKeys={[store.currentMenu]}
-          // items={menuItems}
+          inlineCollapsed={false}
+        // selectedKeys={[store.currentMenu]}
+        // items={menuItems}
         >
-          <Menu.Item key="home">
-            <Link to="/">首页</Link>
-          </Menu.Item>
-          <Menu.Item key="interview" onClick={showModal}>
-            {
-              !store.isLogin ? 
-              <Link to="/">面试助手</Link> :
-              <Link to="/interview">面试助手</Link>
-            }
-          </Menu.Item>
+          {menuItems_component}
         </Menu>
         {
-          store.isLogin ? 
-          <Dropdown menu={{items}} placement='bottom'>
-            <Avatar 
-              style={{ backgroundColor: '#fde3cf', color: '#f56a00' }} 
-              className='login-btn'
-            >
-                { store.nickName }
-            </Avatar>
-          </Dropdown> :
-          <>
-            <Button className='login-btn' onClick={showModal}>登陆</Button>
-            <Login />
-          </>
+          store.isLogin ?
+            <Dropdown menu={{ items }} placement='bottom'>
+              <Avatar
+                style={{ backgroundColor: '#fde3cf', color: '#f56a00' }}
+                className='login-btn'
+              >
+                {store.nickName}
+              </Avatar>
+            </Dropdown> :
+            <>
+              <Button type='default' className='login-btn' onClick={showModal}>登录</Button>
+              <Login />
+            </>
         }
       </div>
     </Header>
