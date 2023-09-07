ad9c8bcd48b8c92aa5262eff2532f830b16a9a50
vincenteMBP <vincenteliang@foxmail.com>
2023-09-07 17:09:14 +0800

    feat: 面经卡片组件

---
+++ src/components/RecordCard.jsx
@@ -0,0 +1,32 @@
+import { Card, Meta, Avatar } from "antd"
+import aliyunUrl from '../../imgs/aliyun.jpg'
+
+const RecordCard = () => {
+  // TODO 统一接口格式，替换静态数据
+  // const {category, company, department, direction, interview_time } = item;
+
+  return (
+    <Card className="record-card"
+      hoverable
+      cover={<img alt="aliyun" src={aliyunUrl} />}
+    >
+      <Meta title="阿里云2024面试" />
+      <div className='interview-info'>
+        <div className='info'>Golang开发</div>
+        <div className='info'>2h10min</div>
+      </div>
+      <Meta
+        description="简单介绍一下MySQL的基本原理？是否了解MySQL索引呢？请你简单介绍一下什么场景..."
+      />
+      <div className='interview-footer'>
+        <div className='interview-footer-user'>
+          <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+          <div>userName</div>
+        </div>
+        <div className='interview-footer-star'>★25</div>
+      </div>
+    </Card>
+  )
+};
+
+export default RecordCard;
