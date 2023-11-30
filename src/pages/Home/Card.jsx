a55fefe58cc2a677cd1b72d8865c555ccfdd982c
LzmUSTB <u202142025@xs.ustb.edu.cn>
2023-11-30 15:59:00 +0800

    首页卡片进入模拟面试

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -8,17 +8,17 @@ import heart_img from "../../imgs/icon_heart.png"
 
 
 const style = {
-  轻松:{
-    bg:"bg-yellow-500",
-    text:'轻松'
+  轻松: {
+    bg: "bg-yellow-500",
+    text: '轻松'
   },
-  严肃:{
-    bg:"bg-[#EA486A]",
-    text:"严肃"
+  严肃: {
+    bg: "bg-[#EA486A]",
+    text: "严肃"
   },
-  标准:{
-    text:'标准',
-    bg:"bg-[#5493FE]"
+  标准: {
+    text: '标准',
+    bg: "bg-[#5493FE]"
   }
 }
 
@@ -29,7 +29,7 @@ export const ShareCard = (props) => {
 
 
   return (
-    <div className='px-2 w-[400px] mx-2'>
+    <div className='px-2 w-[400px] mx-2' onClick={props.onClick}>
       <div className='bg-white p-2 rounded-lg h-full card-item group'>
         <div className='flex items-center justify-between py-2'>
           <div className='flex items-center '>
@@ -40,25 +40,25 @@ export const ShareCard = (props) => {
             </div>
           </div>
           <div className='text-neutral-600'>
-             <span className='text-[#333] group-hover:text-white group-hover:block '><LoginOutlined  /> 进入模拟面试</span>
-            
+            <span className='text-[#333] group-hover:text-white group-hover:block '><LoginOutlined /> 进入模拟面试</span>
+
           </div>
         </div>
-        <div className='mt-3 mb-2 flex justify-between'><p><span className='font-bold text-md'>面试风格</span> <span className={`${style[item.style].bg} rounded-lg text-white px-2 pt-[1px] pb-[2px]  text-[10px]`} >{style[item.style].text}</span></p><p className="text-neutral-600">{new Date(item.interview_date * 1000).toLocaleDateString().replaceAll("/",'-')}</p></div>
+        <div className='mt-3 mb-2 flex justify-between'><p><span className='font-bold text-md'>面试风格</span> <span className={`${style[item.style].bg} rounded-lg text-white px-2 pt-[1px] pb-[2px]  text-[10px]`} >{style[item.style].text}</span></p><p className="text-neutral-600">{new Date(item.interview_date * 1000).toLocaleDateString().replaceAll("/", '-')}</p></div>
         <p className='text-neutral-600 line-clamp-3 h-[60px]'>{item.brief}</p>
         <div className='flex justify-between mt-4 items-center' >
           <p className="flex items-center">
             <img className="w-[30px] rounded-3xl mr-1" src={item.avatar} alt="" />
-            <span className='text-[#333] '> 
+            <span className='text-[#333] '>
               {item.nick_name}
             </span>
           </p>
           <p>
             <span className='text-[#333] ml-2'><LikeOutlined /> {item.popularity_rating}</span>
             <span className='text-[#333] ml-2'>
-              { item.is_favorite ?  <img src={heart_img} style={{display:"inline", width:"16px", position:"relative", top:"-1px"}}/>: <HeartOutlined onClick={() => {props.like && props.like(item)}}/>  }
+              {item.is_favorite ? <img src={heart_img} style={{ display: "inline", width: "16px", position: "relative", top: "-1px" }} /> : <HeartOutlined onClick={() => { props.like && props.like(item) }} />}
               &nbsp;{item.favorites}
-              </span>
+            </span>
             <span className='text-[#333] ml-2'><WarningOutlined /> </span>
           </p>
         </div>
@@ -70,7 +70,7 @@ export const ShareCard = (props) => {
 export const CommentCard = (props) => {
   const item = props.dataSource
   return (
-    <div className={`mr-4 bg-white py-3 px-2 mx-1 w-[420px] h-[80] rounded-lg shadow-md ${props.shadowColor}`} >
+    <div onClick={props.onClick} className={`mr-4 bg-white py-3 px-2 mx-1 w-[420px] h-[80] rounded-lg shadow-md ${props.shadowColor}`} >
       <div className='w-full mb-1 flex items-center bg-wh'>
         <img src={item.avatar} className='w-[70px] h-[70px] rounded-[50%] mr-2' alt="" />
         <div>
