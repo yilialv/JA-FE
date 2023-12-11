83a68cfbba8c31f0a8366bec1789694693c93751
kunpeng <2980967753@qq.com>
2023-12-11 20:03:07 +0800

    123

---
+++ src/pages/AssistantInterview/Card.jsx
@@ -0,0 +1,91 @@
+import { HeartOutlined, LoginOutlined, WarningOutlined, LikeOutlined } from "@ant-design/icons"
+import heart_img from "../../imgs/icon_heart.png"
+import person_img from "../../imgs/person.svg"
+
+
+const style = {
+  轻松: {
+    bg: "bg-yellow-500",
+    text: '轻松'
+  },
+  严肃: {
+    bg: "bg-[#EA486A]",
+    text: "严肃"
+  },
+  标准: {
+    text: '标准',
+    bg: "bg-[#5493FE]"
+  },
+  0: {
+    bg: "bg-yellow-500",
+    text: '轻松'
+  },
+}
+
+
+export const ShareCard = (props) => {
+
+  const item = props.dataSource
+
+
+  return (
+    <div className='px-2 w-[400px]  mx-1' onClick={props.onClick}>
+      <div className='bg-white p-2 rounded-lg h-full card-item group'>
+        <div className='flex items-center justify-between py-2'>
+          <div className='flex items-center '>
+            <img src={item.logo} className="w-[55px] h-[55px] rounded-[50%] mr-2" alt="" />
+            <div>
+              <p className='font-bold text-[18px]'>{item.company}</p>
+              <p className='text-neutral-600 '>{item.direction}</p>
+            </div>
+          </div>
+        
+          <div style={{display:props.children ? 'block' : 'none'}}>
+            {props.children}
+          </div>
+        </div>
+        <div className='mt-3 mb-2 flex justify-between'><p><span className='font-bold text-md'>面试风格</span> <span className={`${style[(item.style || '轻松')].bg} rounded-lg text-white px-2 pt-[1px] pb-[2px]  text-[10px]`} >{style[(item.style || '轻松')].text}</span></p><p className="text-neutral-600">{new Date(item.interview_date * 1000).toLocaleDateString().replaceAll("/", '-')}</p></div>
+        <p className='text-neutral-600 line-clamp-3 h-[60px]'>{item.brief}</p>
+        <div className='flex justify-between mt-4 items-center' >
+          <p className="flex items-center">
+            <img className="w-[30px] rounded-3xl mr-1" src={item.avatar} alt="" />
+            <span className='text-[#333] '>
+              {item.nick_name}
+            </span>
+          </p>
+          <p className="flex">
+            <span className='text-[#333] ml-2 flex'><img className="w-[15px] relative left-[-3px] top-[-2px]" src={person_img} alt="" /> {item.popularity_rating}</span>
+            <span className='text-[#333] ml-2'>
+              {item.is_favorite ? <img src={heart_img} style={{ display: "inline", width: "16px", position: "relative", top: "-1px" }} /> : <HeartOutlined onClick={() => { props.like && props.like(item) }} />}
+              &nbsp;{item.favorites}
+            </span>
+            <span className='text-[#333] ml-2'><WarningOutlined /> </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const CommentCard = (props) => {
+  const item = props.dataSource
+  return (
+    <div onClick={props.onClick} className={`mr-4 bg-white py-3 px-2 mx-1 w-[420px] h-[80] rounded-lg shadow-md ${props.shadowColor}`} >
+      <div className='w-full mb-1 flex items-center bg-wh'>
+        <img src={item.avatar} className='w-[70px] h-[70px] rounded-[50%] mr-2' alt="" />
+        <div>
+          <p className="w-full mb-1 flex items-center justify-between">
+            <span className='font-bold'>{item.nickName}</span>
+            <span className='text-[#333]'>{item.time}</span>
+
+          </p>
+          <p className='text-[#333] line-clamp-3 whitespace-break-spaces '>
+            {item.desc}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
--- src/pages/AssistantInterview/index.jsx
+++ src/pages/AssistantInterview/index.jsx
@@ -1,10 +1,14 @@
 import img_bg from "../../imgs/bg.png"
+import img_filter from "../../imgs/filter.svg"
 import CustomInput from "../../components/Input/Input";
 import { useEffect, useState } from "react";
-import { Button, Modal, Form, Select, Radio, Input, message } from "antd";
+import locale from 'antd/es/date-picker/locale/zh_CN';
+
+import 'dayjs/locale/zh-cn';
+import { Button, Modal, Form, Select, Radio, Input, DatePicker  } from "antd";
 import store from "../../store";
 import "./index.less"
-import {ShareCard}  from "../Home/Card"
+import { ShareCard } from "./Card"
 import axios from "axios";
 import { BASE_URL } from "../../constant";
 
@@ -12,36 +16,34 @@ const AssistantInterview = () => {
   const [search, setSearch] = useState({
     company: ""
   })
-  const [cardList,setCardList] = useState([])
-
+  const [cardList, setCardList] = useState([])
+  const [activeIndex, setActiveIndex] = useState(0)
   useEffect(() => {
     axios.post(`${BASE_URL}/api/experience/mock_interview_hall`, {
-      "company": "",
-      "direction": "",
+
       "limit": 10,
       "page": 1,
-      "round": "1",
-      "sort_type": 0
+
     }).then(res => {
-      const {data, code} = res.data
-      const farmatCompanyList = store.getFormatCompanyList(data.company_info)   
-      setCompanyList(farmatCompanyList)
+      const { data, code } = res.data
+      setCardList(data.mock_interview_cards)
     })
   })
   return (
     <div className="assistant-interview">
-      <div className="w-[1440px] mx-auto">
+      <div className="w-[1200px] mx-auto">
         <img src={img_bg} className="w-full" alt="" />
       </div>
-      <div className="w-[1440px] mx-auto py-8 flex">
+      <div className="w-[1200px] mx-auto py-8 pb-4  flex">
         <div className="flex items-center">
           <span className="font-bold">公司：</span>
           <CustomInput value={search.company} dataList={store.getFormatCompanyList()} callback={(value) => { }} />
         </div>
-        <div className="flex items-center ml-4">
+        <div className="flex items-center ml-5">
           <span className="font-bold">岗位方向：</span>
+
           <Select
-            className="min-[200px]"
+           
             showSearch
             placeholder='请选择您的岗位方向'
             rules={[{ required: true }]}
@@ -72,20 +74,33 @@ const AssistantInterview = () => {
             ]}
           />
         </div>
-        <div className="flex items-center ml-4">
-          <span className="font-bold w-[120px] ">面试轮数：</span>
-          <Radio.Group
-            options={[1, 2, 3, 4, 5, 'HR']}
-            optionType='button'
-            size='small'
-            style={{ width: "100%" }}
-            rules={[{ required: true }]}
-          />
+        <div className="flex items-center ml-5">
+          <span className="font-bold w-[80px] ">面试轮数：</span>
+          <ul className="flex rounded-[10px] overflow-hidden border bg-white font-bold">
+            {
+              [1, 2, 3, 4, 5, 'HR'].map((item,index) => <li key={index} onClick={() => setActiveIndex(index)} className={`px-3 cursor-pointer py-[8px] ${index == activeIndex ? 'liActve' : ''}`}>{item}</li>)
+            }
+          </ul>
+        </div>
+        <div className="flex items-center ml-4 relative">
+          <span>
+            <img src={img_filter} className="w-[15px]" alt="" />
+          </span>
+          <span className="font-bold ml-1">时间筛选</span>
+          <DatePicker className="absolute opacity-0" picker="month" locale={locale} />
         </div>
       </div>
-      <div className="w-[1440px] mx-auto py-3 flex flex-wrap">
+      <div className="w-[1200px] mx-auto flex justify-end  pr-5">
+          <ul className="filter-ul border">
+            <li className="active">热度最高</li>
+            <li className="noActive">最新发布</li>
+          </ul>
+      </div>
+      <div className="w-[1200px] mx-auto py-3 flex flex-wrap">
         {
-           cardList.map(item => <ShareCard key={item.id} like={like} dataSource={item} />)
+          cardList.map(item => <ShareCard key={item.id} like={() => { }} dataSource={item} />
+
+          )
         }
       </div>
     </div>
